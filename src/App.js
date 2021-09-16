ccfec7b449111961419bf0598cea26a13d610aa4
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-09-16 17:11:58 +0300

    hooks v.2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import { Notify } from "notiflix";
 import Loader from "react-loader-spinner";
 
@@ -11,125 +11,226 @@ import ImageGalleryItem from "./components/ImageGalleryItem";
 import Button from "./components/Button";
 import Modal from "./components/Modal";
 
-class App extends Component {
-  state = {
-    images: [],
-    name: "",
-    page: null,
-    showLoader: false,
-    hits: 0,
-    showModal: false,
-    modalSrc: "",
-  };
+export default function App(params) {
+  const [images, setImages] = useState([]);
+  const [name, setName] = useState("");
+  const [page, setpage] = useState(1);
+  const [showLoader, setShowLoader] = useState(false);
+  const [hits, setHits] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [modalSrc, setModalSrc] = useState("");
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      this.state.name !== prevState.name ||
-      this.state.page !== prevState.page
-    ) {
-      this.dataRequestAPI();
+  useEffect(() => {
+    if (!name) {
+      return;
     }
-  }
 
-  dataRequestAPI = () => {
-    const { name, page } = this.state;
-    this.toogleLoader(true);
+    toogleLoader(true);
     fetchImages(name, page)
-      .then((images) => {
-        if (images.data.hits.length === 0) {
+      .then((totalImages) => {
+        if (totalImages.data.hits.length === 0) {
           Notify.failure(
             "Sorry, there are no images matching your search query. Please try again."
           );
         }
 
-        this.setState((prevState) => {
-          return {
-            images: [...prevState.images, ...images.data.hits],
-            hits: prevState.hits + images.data.hits.length,
-          };
-        });
+        setImages((prevImages) => [...prevImages, ...totalImages.data.hits]);
+        setHits((prevHits) => prevHits + totalImages.data.hits.length);
 
-        if (this.state.hits >= images.data.totalHits) {
-          this.setState({ hits: 0 });
+        if (hits >= totalImages.data.totalHits) {
+          setHits(0);
         }
-        this.scrollToBottom();
+
+        scrollToBottom();
       })
       .catch((error) => {
         Notify.failure(
           "Sorry, there are no images matching your search query. Please try again."
         );
       })
-      .finally(() => this.toogleLoader(false));
-  };
+      .finally(() => toogleLoader(false));
+  }, [name, page]);
 
-  scrollToBottom = () => {
+  const scrollToBottom = () => {
     window.scrollTo({
       top: document.documentElement.scrollHeight,
       behavior: "smooth",
     });
   };
 
-  writeValuesToState = (data) => {
-    data.image
-      ? this.setState({
-          images: [],
-          name: data.image,
-          page: data.page,
-          hits: data.hits,
-        })
-      : this.setState({ page: data.page, hits: data.hits });
+  const writeValuesToState = (data) => {
+    if (data.image) {
+      setImages([]);
+      setName(data.image);
+      setpage(data.page);
+      setHits(data.hits);
+    } else {
+      setpage(data.page);
+      setHits(data.hits);
+    }
   };
 
-  toogleLoader = (status) => {
-    return this.setState({ showLoader: status });
+  const toogleLoader = (status) => {
+    return setShowLoader(status);
   };
 
-  toogleModal = (data) => {
-    this.setState({
-      showModal: data,
-    });
+  const toogleModal = (data) => {
+    setShowModal(data);
   };
 
-  writeSrcState = (data) => {
-    this.setState({
-      modalSrc: data,
-    });
+  const writeSrcState = (data) => {
+    setModalSrc(data);
   };
 
-  render() {
-    const { images, page, hits, showModal, modalSrc } = this.state;
-
-    return (
-      <div className="App">
-        <Searchbar onSubmit={this.writeValuesToState} />
-        <ImageGallery>
-          <ImageGalleryItem
-            imageGallery={images}
-            toogleModal={this.toogleModal}
-            writeSrcState={this.writeSrcState}
-          />
-        </ImageGallery>
-        {this.state.showLoader && (
-          <Loader
-            type="Audio"
-            color="#3f51b5"
-            height={80}
-            width={80}
-            timeout={500}
-            className="Loader"
-          />
-        )}
-        <Button
-          totalHits={hits}
-          onSubmit={this.writeValuesToState}
-          currentPage={page}
+  return (
+    <div className="App">
+      <Searchbar onSubmit={writeValuesToState} />
+      <ImageGallery>
+        <ImageGalleryItem
+          imageGallery={images}
+          toogleModal={toogleModal}
+          writeSrcState={writeSrcState}
         />
-        {showModal && (
-          <Modal modalSrc={modalSrc} toogleModal={this.toogleModal} />
-        )}
-      </div>
-    );
-  }
+      </ImageGallery>
+      {showLoader && (
+        <Loader
+          type="Audio"
+          color="#3f51b5"
+          height={80}
+          width={80}
+          timeout={500}
+          className="Loader"
+        />
+      )}
+      <Button
+        totalHits={hits}
+        onSubmit={writeValuesToState}
+        currentPage={page}
+      />
+      {showModal && <Modal modalSrc={modalSrc} toogleModal={toogleModal} />}
+    </div>
+  );
 }
 
-export default App;
+// class App extends Component {
+//   state = {
+//     images: [],
+//     name: "",
+//     page: null,
+//     showLoader: false,
+//     hits: 0,
+//     showModal: false,
+//     modalSrc: "",
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (
+//       this.state.name !== prevState.name ||
+//       this.state.page !== prevState.page
+//     ) {
+//       this.dataRequestAPI();
+//     }
+//   }
+
+//   dataRequestAPI = () => {
+//     const { name, page } = this.state;
+//     this.toogleLoader(true);
+//     fetchImages(name, page)
+//       .then((images) => {
+//         if (images.data.hits.length === 0) {
+//           Notify.failure(
+//             "Sorry, there are no images matching your search query. Please try again."
+//           );
+//         }
+
+//         this.setState((prevState) => {
+//           return {
+//             images: [...prevState.images, ...images.data.hits],
+//             hits: prevState.hits + images.data.hits.length,
+//           };
+//         });
+
+//         if (this.state.hits >= images.data.totalHits) {
+//           this.setState({ hits: 0 });
+//         }
+//         this.scrollToBottom();
+//       })
+//       .catch((error) => {
+//         Notify.failure(
+//           "Sorry, there are no images matching your search query. Please try again."
+//         );
+//       })
+//       .finally(() => this.toogleLoader(false));
+//   };
+
+//   scrollToBottom = () => {
+//     window.scrollTo({
+//       top: document.documentElement.scrollHeight,
+//       behavior: "smooth",
+//     });
+//   };
+
+//   writeValuesToState = (data) => {
+//     data.image
+//       ? this.setState({
+//           images: [],
+//           name: data.image,
+//           page: data.page,
+//           hits: data.hits,
+//         })
+//       : this.setState({ page: data.page, hits: data.hits });
+//   };
+
+//   toogleLoader = (status) => {
+//     return this.setState({ showLoader: status });
+//   };
+
+//   toogleModal = (data) => {
+//     this.setState({
+//       showModal: data,
+//     });
+//   };
+
+//   writeSrcState = (data) => {
+//     this.setState({
+//       modalSrc: data,
+//     });
+//   };
+
+//   render() {
+//     const { images, page, hits, showModal, modalSrc } = this.state;
+
+//     return (
+//       <div className="App">
+//         <Searchbar onSubmit={this.writeValuesToState} />
+//         <ImageGallery>
+//           <ImageGalleryItem
+//             imageGallery={images}
+//             toogleModal={this.toogleModal}
+//             writeSrcState={this.writeSrcState}
+//           />
+//         </ImageGallery>
+//         {this.state.showLoader && (
+//           <Loader
+//             type="Audio"
+//             color="#3f51b5"
+//             height={80}
+//             width={80}
+//             timeout={500}
+//             className="Loader"
+//           />
+//         )}
+//         <Button
+//           totalHits={hits}
+//           onSubmit={this.writeValuesToState}
+//           currentPage={page}
+//         />
+//         {showModal && (
+//           <Modal modalSrc={modalSrc} toogleModal={this.toogleModal} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
